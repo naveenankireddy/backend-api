1b2afaf6a0041dff2327e6815fbc011a26c07464
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-12-01 22:15:49 +0530

    connecte to mongodb and create cluster logic done

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+const { MONGOURI } = require("./keys");
+const port = process.env.PORT || 5000;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//mongodb connection
+
+mongoose.connect(
+  MONGOURI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  },
+  (err) => {
+    console.log("connected", err ? err : true);
+  }
+);
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
+app.listen(port, () => {
+  console.log("Server is started at", port, "CC dug dug dug dug dug.......");
+});
 module.exports = app;
