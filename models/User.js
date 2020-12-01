45246b3f31e06ace10e374bc92e6823fbbcc1592
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-12-01 22:54:55 +0530

    userScema with geolocation done

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const GeoSchema = new Schema({
+  type: {
+    type: String,
+    default: "Point",
+  },
+  coordinates: {
+    type: [Number],
+    index: "2dsphere",
+  },
+});
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    address: {
+      street: {
+        type: String,
+        required: true,
+      },
+      locality: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: String,
+        required: true,
+      },
+      geometry: GeoSchema,
+    },
+  },
+  { timestamps: true }
+);
